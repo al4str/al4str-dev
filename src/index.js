5c06bf6201d4d4b03ce0c640f9b1b5c262661fb6
al4str <al4str.ftw@gmail.com>
2021-05-09 19:35:50 +0300

    simplify parallax even more

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 (function() {
   const PREFERS_QUERY = '(prefers-reduced-motion: reduce)';
   const REDUCED_MOTION = window.matchMedia(PREFERS_QUERY).matches;
-  /** @type {HTMLImageElement} */
-  const PARALLAX_IMAGE = window.document.getElementById('parallax-image');
+  /** @type {HTMLDivElement} */
+  const PARALLAX_IMAGE = window.document.querySelector('.parallax__image');
   const PARALLAX_PARAMS = {
     scrollTop: window.scrollY,
     scrollHeight: window.document.body.scrollHeight,
-    viewportWidth: window.innerWidth,
     viewportHeight: window.innerHeight,
-    imageWidth: 0,
-    imageHeight: 0,
+    imageHeight: 2500,
   };
 
   /**
@@ -69,7 +67,6 @@
 
   function updateParallaxParams() {
     PARALLAX_PARAMS.scrollHeight = window.document.body.scrollHeight;
-    PARALLAX_PARAMS.viewportWidth = window.innerWidth;
     PARALLAX_PARAMS.viewportHeight = window.innerHeight;
   }
 
@@ -77,14 +74,11 @@
     const {
       scrollTop,
       scrollHeight,
-      viewportWidth,
       viewportHeight,
-      imageWidth,
       imageHeight,
     } = PARALLAX_PARAMS;
-    const x = viewportWidth / 2 - imageWidth / 2;
     const y = -1 * (imageHeight - viewportHeight) * (scrollTop / scrollHeight);
-    PARALLAX_IMAGE.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+    PARALLAX_IMAGE.style.transform = `translate3d(0, ${y}px, 0)`;
   }
 
   function handleResize() {
@@ -95,16 +89,6 @@
   function handleScroll() {
     PARALLAX_PARAMS.scrollTop = window.scrollY;
     window.requestAnimationFrame(updateParallaxPosition);
-  }
-
-  function handleParallaxImageLoad() {
-    const { naturalWidth, naturalHeight } = PARALLAX_IMAGE;
-    PARALLAX_PARAMS.imageWidth = naturalWidth;
-    PARALLAX_PARAMS.imageHeight = naturalHeight;
-    PARALLAX_IMAGE.width = naturalWidth;
-    PARALLAX_IMAGE.height = naturalHeight;
-    updateParallaxPosition();
-    PARALLAX_IMAGE.parentElement.classList.add('parallax__wrapper_loaded');
   }
 
   /** @param {MouseEvent} e */
@@ -127,17 +111,11 @@
     .forEach((link) => link.onclick = handleLinkClick);
   window.addEventListener('resize', handleResize, {
     passive: true,
-    capture: true,
   });
   if (!REDUCED_MOTION) {
     window.addEventListener('scroll', handleScroll, {
       passive: true,
-      capture: true,
     });
-    PARALLAX_IMAGE.onload = handleParallaxImageLoad;
-    if (PARALLAX_IMAGE.complete) {
-      handleParallaxImageLoad();
-    }
     updateParallaxPosition();
   }
 }());
